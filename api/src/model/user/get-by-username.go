eb3739e64b90eb9fa29da969c6c30739583b5709
ise0 <isengard99@yandex.ru>
2023-07-28 14:15:45 +0300

    fix user rating display bug

--- api/src/model/user/get-by-username.go
+++ api/src/model/user/get-by-username.go
@@ -12,8 +12,8 @@ import (
 func GetByUsername(username string) (user, error) {
 	var u user
 	if err := db.DB.QueryRow(context.TODO(), `
-		select user_id, is_anonym, username, user_password from users where username = $1
-	`, username).Scan(&u.Id, &u.IsAnonym, &u.Username, &u.Password); err != nil {
+		select user_id, is_anonym, username, rating, user_password from users where username = $1
+	`, username).Scan(&u.Id, &u.IsAnonym, &u.Username, &u.Rating, &u.Password); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return user{}, fmt.Errorf("user with username:%s does not exists", username)
 		}
